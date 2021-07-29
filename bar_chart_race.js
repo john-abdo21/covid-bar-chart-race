bda722e5c1369f2c18c4db7ddf8914bd1737f1a3
siritimmy <88086854+siritimmy@users.noreply.github.com>
2021-07-29 17:27:18 +0700

    Update bar_chart_race.js

--- bar_chart_race.js
+++ bar_chart_race.js
@@ -225,7 +225,7 @@ function createBarChartRace(data) {
       .text(formatDate(keyframes[0][0]));
 
     return ([date], transition) => {
-      transition.end().then(() => now.text(formatDate(date));
+      transition.end().then(() => now.text(formatDate(date)));
     };
   }
   
